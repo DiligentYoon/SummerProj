650063fdb3cada6ecef09597e5bc76fb4bf6ecbd
Home <grape4314@cau.ac.kr>
2025-07-12 22:54:09 +0900

    strat to learn motion primitives : grasp

---
+++ source/SummerProj/SummerProj/tasks/direct/franka_pap/franka_grasp_env_cfg.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import  RigidObjectCfg
+from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
+from isaaclab.sim import SimulationCfg
+from isaaclab.markers import VisualizationMarkersCfg
+from isaaclab.utils import configclass
+from isaaclab.markers.config import FRAME_MARKER_CFG
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from .franka_base_env_cfg import FrankaBaseEnvCfg
+
+
+@configclass
+class FrankaGraspEnvCfg(FrankaBaseEnvCfg):
+    # env
+    episode_length_s = 10.0
+    decimation = 10
+    action_space = 20
+    observation_space = 35
+    state_space = 0
+
+    # simulation
+    sim: SimulationCfg = SimulationCfg(
+        dt=1 / 100,
+        render_interval=decimation,
+        physics_material=sim_utils.RigidBodyMaterialCfg(
+            friction_combine_mode="multiply",
+            restitution_combine_mode="multiply",
+            static_friction=1.0,
+            dynamic_friction=1.0,
+            restitution=0.0,
+        ),
+    )
+
+    # object
+    object: RigidObjectCfg = RigidObjectCfg(
+        prim_path="/World/envs/env_.*/Object",
+        init_state=RigidObjectCfg.InitialStateCfg(pos=[0.35, 0.0, 0.0], rot=[1.0, 0.0, 0.0, 0.0]),
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
+                scale=(1.0, 1.0, 1.0),
+                rigid_props=RigidBodyPropertiesCfg(
+                    solver_position_iteration_count=16,
+                    solver_velocity_iteration_count=1,
+                    max_angular_velocity=1000.0,
+                    max_linear_velocity=1000.0,
+                    max_depenetration_velocity=5.0,
+                    disable_gravity=False,
+                ),
+            ),
+        )
+    
+    # goal marker
+    goal_pos_marker_cfg: VisualizationMarkersCfg = FRAME_MARKER_CFG.replace(
+        prim_path="Visuals/goal_marker",
+        markers={
+        "frame": sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/frame_prim.usd",
+            scale=(0.1, 0.1, 0.1),
+            )
+        }
+    )
+    
+
+    # reward hyperparameter
+    alpha, beta = 3.0, 4.0
+    w_pos = 50.0
+    w_rot = 25.0
+    w_penalty = 0.01
+    w_success = 10.0
