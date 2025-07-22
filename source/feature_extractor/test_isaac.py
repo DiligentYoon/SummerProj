069279e36d40f3dab74a04fa281ee6c99cf5e0b6
AISL <grape4314@cau.ac.kr>
2025-07-22 14:11:22 +0900

    test.ver2

--- source/feature_extractor/test_isaac.py
+++ source/feature_extractor/test_isaac.py
@@ -22,12 +22,13 @@ We add the following sensors on the quadruped robot, ANYmal-C (ANYbotics):
 """Launch Isaac Sim Simulator first."""
 
 import argparse
-
+import os
 from isaaclab.app import AppLauncher
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Tutorial on adding sensors on a robot.")
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
+# parser.add_argument("--enable_cameras", type=bool, default=True)
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -42,7 +43,8 @@ simulation_app = app_launcher.app
 import torch
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
+from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg, CollisionPropertiesCfg, MassPropertiesCfg
 from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from isaaclab.sensors import CameraCfg
@@ -55,11 +57,55 @@ RIGHT_ROT = (0.18761, 0.13613, -0.53848, -0.81013)
 
 # Experimental known (Configuration -> Simulation Transformation)
 QUAT_CON_TO_SIM = (0.5, 0.5, -0.5, -0.5)
+# Inverse Quaternion (w, -x, -y, -z)
 QUAT_SIM_TO_CON = (0.5, -0.5, 0.5, 0.5)
 
+# Tranformation from setted simulation quat to offset configuration thing
 FRONT_ROT_CON = tuple(quat_mul(torch.tensor(FRONT_ROT), torch.tensor(QUAT_SIM_TO_CON)).numpy())
 LEFT_ROT_CON = tuple(quat_mul(torch.tensor(LEFT_ROT), torch.tensor(QUAT_SIM_TO_CON)).numpy())
 RIGHT_ROT_CON = tuple(quat_mul(torch.tensor(RIGHT_ROT), torch.tensor(QUAT_SIM_TO_CON)).numpy())
+
+OBJECT_DIR = {
+    "mug_1": {
+        "url": "/025_mug.usd",
+        "class": "mug"
+    },
+
+    "mug_2": {
+        "url": "/SM_Mug_B1.usd",
+        "class": "mug"
+    },
+
+    "mug_3": {
+        "url": "/SM_Mug_C1.usd",
+        "class": "mug"
+    },
+
+    "mug_4": {
+        "url": "/SM_Mug_D1.usd",
+        "class": "mug"
+    },
+
+    "cube_1": {
+        "url": "/010_potted_meat_can.usd",
+        "class": "cube"
+    },
+
+    "cube_2": {
+        "url": "/003_cracker_box.usd",
+        "class": "cube"
+    },
+
+    "cylinder_1": {
+        "url": "/002_master_chef_can.usd",
+        "class": "cylinder"
+    },
+
+    "cylinder_2": {
+        "url": "/005_tomato_soup_can.usd",
+        "class": "cylinder"
+    },
+}
 
 @configclass
 class SensorsSceneCfg(InteractiveSceneCfg):
@@ -96,8 +142,8 @@ class SensorsSceneCfg(InteractiveSceneCfg):
             focal_length=24.0,
             focus_distance=400.0,
             horizontal_aperture=20.954999923706055,
-            vertical_aperture=15.290800094604492,  # 이 값을 명시적으로 추가합니다.
-            clipping_range=(1.0, 1000000.0)      # Near/Far 값을 정확히 맞춰줍니다.
+            vertical_aperture=15.290800094604492,  
+            clipping_range=(1.0, 1000000.0)   
         ),
         offset=CameraCfg.OffsetCfg(pos=(1.6, 0.0, 0.7), rot=FRONT_ROT_CON, convention="world"),
     )
@@ -114,8 +160,8 @@ class SensorsSceneCfg(InteractiveSceneCfg):
             focal_length=24.0,
             focus_distance=400.0,
             horizontal_aperture=20.954999923706055,
-            vertical_aperture=15.290800094604492,  # 이 값을 명시적으로 추가합니다.
-            clipping_range=(1.0, 1000000.0)      # Near/Far 값을 정확히 맞춰줍니다.
+            vertical_aperture=15.290800094604492,  
+            clipping_range=(1.0, 1000000.0)   
         ),
         offset=CameraCfg.OffsetCfg(pos=(-0.13, -1.2, 0.5), rot=LEFT_ROT_CON, convention="world"),
     )
@@ -138,6 +184,26 @@ class SensorsSceneCfg(InteractiveSceneCfg):
         offset=CameraCfg.OffsetCfg(pos=(-0.13, 1.2, 0.5), rot=RIGHT_ROT_CON, convention="world"),
     )
 
+    # object
+    object: RigidObjectCfg = RigidObjectCfg(
+        prim_path="/World/envs/env/Object",
+        init_state=RigidObjectCfg.InitialStateCfg(pos=[0.4, 0.0, 0.0], rot=[1.0, 0.0, 0.0, 0.0]),
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=os.path.join(os.getcwd(), "Dataset", "mydata") + OBJECT_DIR["mug_2"]["url"],
+                collision_props=CollisionPropertiesCfg(collision_enabled=True),
+                scale=(0.01, 0.01, 0.01),
+                rigid_props=RigidBodyPropertiesCfg(
+                    solver_position_iteration_count=16,
+                    solver_velocity_iteration_count=1,
+                    max_angular_velocity=1000.0,
+                    max_linear_velocity=1000.0,
+                    max_depenetration_velocity=5.0,
+                    disable_gravity=False,
+                ),
+            ),
+        )
+    
+
 
 
 def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
@@ -153,38 +219,25 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
         if count % 500 == 0:
             # reset counter
             count = 0
-            # reset the scene entities
-            # root state
-            # we offset the root state by the origin since the states are written in simulation world frame
-            # if this is not done, then the robots will be spawned at the (0, 0, 0) of the simulation world
-            # root_state = scene["robot"].data.default_root_state.clone()
-            # root_state[:, :3] += scene.env_origins
-            # scene["robot"].write_root_pose_to_sim(root_state[:, :7])
-            # scene["robot"].write_root_velocity_to_sim(root_state[:, 7:])
-            # # set joint positions with some noise
-            # joint_pos, joint_vel = (
-            #     scene["robot"].data.default_joint_pos.clone(),
-            #     scene["robot"].data.default_joint_vel.clone(),
-            # )
-            # joint_pos += torch.rand_like(joint_pos) * 0.1
-            # scene["robot"].write_joint_state_to_sim(joint_pos, joint_vel)
-            # clear internal buffers
+            default_state = scene["object"].data.default_root_state
+            scene["object"].write_root_pose_to_sim(default_state[:, :7])
+            scene["object"].write_root_velocity_to_sim(default_state[:, 7:])
+            scene.write_data_to_sim()
             scene.reset()
             print("[INFO]: Resetting robot state...")
-        # Apply default actions to the robot
-        # -- generate actions/commands
-        # targets = scene["robot"].data.default_joint_pos
-        # # -- apply action to the robot
-        # scene["robot"].set_joint_position_target(targets)
-        # # -- write data to sim
-        # scene.write_data_to_sim()
-        # perform step
+        
         sim.step()
         # update sim-time
         sim_time += sim_dt
         count += 1
         # update buffers
         scene.update(sim_dt)
+
+        data_front = scene["front_camera"].data.output["pointcloud"]
+        data_left = scene["left_behind_camera"].data.output["pointcloud"]
+        data_right = scene["right_behind_camera"].data.output["pointcloud"]
+
+        print(f"{data_front}")
 
         # print information from the sensors
         # print("-------------------------------")
